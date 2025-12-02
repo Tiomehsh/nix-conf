ee3999b6506f043bdc73ffbd716aaf9b21cde9b0
Tiomehsh <likehsh@163.com>
2025-12-02 22:05:11 +0800

    kitty font

--- home/kitty.nix
+++ home/kitty.nix
@@ -4,11 +4,10 @@
     theme = "Catppuccin-Mocha";
     font = {
       name = "Maple Mono NF CN";
-      size = 20;
+      size = 12;
     };
 
     settings = {
-      # --- 核心设置 ---
       shell = "${pkgs.fish}/bin/fish";
       editor = "${pkgs.neovim}/bin/nvim";
 
@@ -17,19 +16,16 @@
       background_opacity = "0.8";
       background_blur = 32;
       window_padding_width = 5;
-      hide_window_decorations = "yes"; # 去掉标题栏
-      confirm_os_window_close = 0; # 关闭时不询问，直接关
+      hide_window_decorations = "yes";
+      confirm_os_window_close = 0;
 
-      # --- 光标 (保留你的拖尾特效) ---
       cursor_blink_interval = 0;
       cursor_trail = 20;
       cursor_trail_decay = "0.0 0.3";
 
-      # --- 交互 ---
       copy_on_select = "yes";
       mouse_hide_wait = "0.5";
 
-      # --- 字体微调 (Maple Mono 特有配置) ---
       "modify_font cell_width" = "100%";
       "modify_font cell_height" = "105%";
       "font_features MapleMono-NF-CN-Regular" = "+cv01 +ss02 +ss04 +ss05 +zero";
@@ -42,13 +38,10 @@
       "ctrl+shift+c" = "copy_to_clipboard";
       "ctrl+shift+v" = "paste_from_clipboard";
 
-      # 字体缩放
       "ctrl+shift+equal" = "change_font_size all +2.0";
       "ctrl+shift+plus" = "change_font_size all +2.0";
       "ctrl+shift+minus" = "change_font_size all -2.0";
-      "ctrl+shift+backspace" = "change_font_size all 0"; # 恢复默认
-
-      # 滚动 (如果你不用鼠标滚轮的话)
+      "ctrl+shift+backspace" = "change_font_size all 0";
       "ctrl+shift+up" = "scroll_line_up";
       "ctrl+shift+down" = "scroll_line_down";
       "ctrl+shift+page_up" = "scroll_page_up";
